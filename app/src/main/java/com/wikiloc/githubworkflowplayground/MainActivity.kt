f9982a1f1f656f0d9198ae296da64b5257e7021c
Manel Martos <manel.martos@wikiloc.com>
2025-01-17 12:08:57 +0100

    Initial commit

---
+++ app/src/main/java/com/wikiloc/githubworkflowplayground/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.wikiloc.githubworkflowplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.wikiloc.githubworkflowplayground.ui.theme.GithubWorkflowPlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      GithubWorkflowPlaygroundTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  GithubWorkflowPlaygroundTheme {
+    Greeting("Android")
+  }
+}
