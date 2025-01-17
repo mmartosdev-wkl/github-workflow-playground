7657563a39588e2cb642522adabf593296ca422c
Manel Martos <manel.martos@wikiloc.com>
2025-01-17 17:26:19 +0100

    Add a new composable: a Button

---
+++ app/src/main/java/com/wikiloc/githubworkflowplayground/composables/Button.kt
@@ -0,0 +1,9 @@
+package com.wikiloc.githubworkflowplayground.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Button(modifier: Modifier = Modifier) {
+
+}
