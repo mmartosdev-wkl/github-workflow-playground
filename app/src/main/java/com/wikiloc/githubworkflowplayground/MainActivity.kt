d22c190a593aecc42450d7a2e5bafdcba4d22902
Manel Martos <manel.martos@wikiloc.com>
2025-01-17 16:52:08 +0100

    Add border

--- app/src/main/java/com/wikiloc/githubworkflowplayground/MainActivity.kt
+++ app/src/main/java/com/wikiloc/githubworkflowplayground/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wikiloc.githubworkflowplayground.ui.theme.GithubWorkflowPlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +41,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
   Text(
     text = "Hello, $name!",
-    modifier = modifier
+    modifier = modifier.border(1.dp, Color.White).padding(24.dp)
   )
 }
 
