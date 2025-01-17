54a4f42c2f721565cf807f9b68839a7f18615731
Manel Martos <manel.martos@wikiloc.com>
2025-01-17 17:53:18 +0100

    Add a new composable: a Chip

---
+++ app/src/main/java/com/wikiloc/githubworkflowplayground/composables/Chip.kt
@@ -0,0 +1,16 @@
+package com.wikiloc.githubworkflowplayground.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Chip(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun PreviewChip() {
+  Chip()
+}
