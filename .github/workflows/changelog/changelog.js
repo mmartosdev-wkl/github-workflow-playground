cf1317b16fd5a709e0ea2e27d7e1eb4341b51ae8
Manel Martos <manel.martos@wikiloc.com>
2025-01-18 08:51:21 +0100

    Add some logs

---
+++ .github/workflows/changelog/changelog.js
@@ -0,0 +1,16 @@
+function getCommitHashFromRef(
+    github,
+    context,
+    ref
+) {
+    const { data: commit } = await github.rest.repos.getCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: ref,
+    });
+    return commit.sha;
+}
+
+module.exports = {
+    getCommitHashFromRef: getCommitHashFromRef
+};
