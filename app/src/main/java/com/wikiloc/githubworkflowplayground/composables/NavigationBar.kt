751cad5a917ac1541f5995f0628f5c3def520b2e
Manel Martos <manel.martos@wikiloc.com>
2025-01-18 23:32:51 +0100

    Dev commit

--- app/src/main/java/com/wikiloc/githubworkflowplayground/composables/NavigationBar.kt
+++ app/src/main/java/com/wikiloc/githubworkflowplayground/composables/NavigationBar.kt
@@ -1,12 +1,16 @@
 package com.wikiloc.githubworkflowplayground.composables
 
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.pm.ShortcutInfoCompat
 
 @Composable
 fun NavigationBar(modifier: Modifier = Modifier) {
-  
+  Surface(modifier = modifier) {
+
+  }
 }
 
 @Preview
