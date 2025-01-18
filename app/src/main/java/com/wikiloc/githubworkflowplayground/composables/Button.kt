c9f7c79aa5c981dca39bad2bb14888c6f5648fdd
Manel Martos <manel.martos@wikiloc.com>
2025-01-19 00:24:44 +0100

    Button with text

--- app/src/main/java/com/wikiloc/githubworkflowplayground/composables/Button.kt
+++ app/src/main/java/com/wikiloc/githubworkflowplayground/composables/Button.kt
@@ -1,13 +1,18 @@
 package com.wikiloc.githubworkflowplayground.composables
 
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun Button(modifier: Modifier = Modifier) {
+fun Button(
+  text: String,
+  modifier: Modifier = Modifier,
+) {
   Surface(modifier) {
+    Text(text = text)
 
   }
 }
@@ -15,5 +20,5 @@ fun Button(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 fun PreviewButton() {
-  Button()
+  Button("Hello!")
 }
