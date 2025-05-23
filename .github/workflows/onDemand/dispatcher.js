685f859f27fb6802bf1df22716cff54fb55e8eb3
Manel Martos <manel.martos@wikiloc.com>
2025-05-23 17:59:55 +0200

    check

--- .github/workflows/onDemand/dispatcher.js
+++ .github/workflows/onDemand/dispatcher.js
@@ -87,7 +87,7 @@ async function dispatchWorkflows({ github, context, commentId, commentBody }) {
      ref: pr.head.ref,
      /* optional, declared in the target workflow’s `on: workflow_dispatch: inputs:` */
      inputs: {
-       taskNames: "build",
+       gradle_tasks: "build",
      }
    });
 }
