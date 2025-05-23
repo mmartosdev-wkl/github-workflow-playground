c5123f4481270a3f9f577264ecabbba31e6c5043
Manel Martos <manel.martos@wikiloc.com>
2025-05-23 16:27:15 +0200

    check

--- .github/workflows/dispatcher/dispatcher.js
+++ .github/workflows/dispatcher/dispatcher.js
@@ -8,7 +8,7 @@ async function dispatchWorkflows({ github, context, commentId, commentBody }) {
     comment_id: commentId,
   });
 
-  // Get data relative to PR where the comment has been added
+  // Get data relative to PR where the comment has been added -> pr.head.sha
   const prNumber = context.issue.number;
   const { data: pr } = await github.rest.pulls.get({
     owner,
@@ -16,6 +16,42 @@ async function dispatchWorkflows({ github, context, commentId, commentBody }) {
     pull_number: prNumber
   });
 
+  // Resolve which gradle tasks should be run
+  let tasksToRun = [];
+  let compatibleTasks = [];
+
+  if (commentBody === "recordScreenshots") {
+    tasksToRun = ["recordScreenshotTests"];
+  } else if (commentBody === "runChecks") {
+    tasksToRun = ["assembleInternalDebug", "detekt", "unitTests", "allTests", "verifyScreenshotTests"];
+  } else if (commentBody.startsWith("runChecks")) {
+    if (commentBody.includes("compile")) {
+      tasksToRun.push("assembleInternalDebug");
+    }
+    if (commentBody.includes("detekt")) {
+      tasksToRun.push("detekt");
+    }
+    if (commentBody.includes("unitTests")) {
+      tasksToRun.push("unitTests", "allTests");
+    }
+    if (commentBody.includes("screenshots")) {
+      tasksToRun.push("verifyScreenshotTests");
+    }
+  }
+
+  // Ensure compatibility of tasks
+  if (tasksToRun.includes("unitTests") || tasksToRun.includes("allTests")) {
+    compatibleTasks.push(
+      tasksToRun.filter(task => task !== "verifyScreenshotTests")
+    );
+  }
+  if (tasksToRun.includes("verifyScreenshotTests")) {
+    compatibleTasks.push(
+      tasksToRun.filter(task => task !== "unitTests" && task !== "allTests")
+    );
+  }
+
+  console.log('Resolved Tasks\n', JSON.stringify(compatibleTasks));
   console.log('PR Data\n' + JSON.stringify(pr));
 }
 
