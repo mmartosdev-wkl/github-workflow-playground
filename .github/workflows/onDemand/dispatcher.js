1df27f049c4602873ea220d870e7e93feab82d18
Manel Martos <manel.martos@wikiloc.com>
2025-05-23 17:47:03 +0200

    check

--- .github/workflows/onDemand/dispatcher.js
+++ .github/workflows/onDemand/dispatcher.js
@@ -74,7 +74,7 @@ async function dispatchWorkflows({ github, context, commentId, commentBody }) {
   // Step 4: Ensure task compatibility
   const compatibleTasks = ensureCompatibleTasks(tasksToRun);
 
-  // console.log(`compatibleTasks: ${JSON.stringify(compatibleTasks)}`);
+  console.log(`compatibleTasks: ${JSON.stringify(compatibleTasks)}`);
 
   // // Step 5: Dispatch workflows
   // await github.rest.actions.createWorkflowDispatch({
