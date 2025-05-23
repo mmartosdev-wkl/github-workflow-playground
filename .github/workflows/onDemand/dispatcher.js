7b74621ba247a5107fad11bd44426ed1ef868dc0
Manel Martos <manel.martos@wikiloc.com>
2025-05-23 17:23:04 +0200

    check

--- .github/workflows/onDemand/dispatcher.js
+++ .github/workflows/onDemand/dispatcher.js
@@ -80,7 +80,7 @@ async function dispatchWorkflows({ github, context, commentId, commentBody }) {
     repo:  repo,
     /** file name *or* numeric ID that you see in the URL on the
         Actions → “...” menu (e.g. `161335`) */
-    workflow_id: ".github/workflows/onDemandWorkflow.yml",
+    workflow_id: "163865713",
     /** branch, tag, or full SHA that the called workflow should run on */
     ref: pr.head.ref,
     /* optional, declared in the target workflow’s `on: workflow_dispatch: inputs:` */
