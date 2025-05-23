f502931a7ce4191638333954cf1db70b08890c52
Manel Martos <manel.martos@wikiloc.com>
2025-05-23 16:13:28 +0200

    check

--- .github/workflows/dispatcher/dispatcher.js
+++ .github/workflows/dispatcher/dispatcher.js
@@ -1,10 +1,22 @@
 async function dispatchWorkflows({ github, context, commentId, commentBody }) {
+  const { owner, repo } = context.repo;
+
   // Remove comment
   await github.rest.issues.deleteComment({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
+    owner: owner,
+    repo: repo,
     comment_id: commentId,
   });
+
+  // Get data relative to PR where the comment has been added
+  const prNumber = context.issue.number;
+  const { data: pr } = await github.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: prNumber
+  });
+
+  console.log('PR Data\n' + pr);
 }
 
 module.exports = {
